aee36f42b193dc77d8ad6591e3cc04712b2c60fc
Junghwan Kim <junghwki@c2r3s6.42seoul.kr>
2022-07-02 02:48:34 +0900

    Utils finish

--- srcs/Utils.hpp
+++ srcs/Utils.hpp
@@ -1,140 +1,166 @@
 #ifndef UTILS_HPP
 # define UTILS_HPP
 
-namespace ft {
-	template < bool _Cond, typename _T = void >
-	class enable_if {
-
+namespace ft
+{
+	template <bool Cond, typename T = void>
+	class enable_if
+	{
 	};
 
-	template < typename _T >
-	class enable_if < true, _T > {
-		public:
-			typedef _T type;
+	template <typename T>
+	class enable_if <true, T>
+	{
+	public:
+		typedef T type;
 	};
 
-	template < typename _T >
-	class is_integral {
-		public:
-			static const bool	value = false;
-	};
-
-	template <>
-	class is_integral < bool > {
-		public:
-			static const bool	value = true;
+	template <typename T>
+	class is_integral
+	{
+	public:
+		static const bool	value = false;
 	};
 
 	template <>
-	class is_integral < char > {
-		public:
-			static const bool	value = true;
+	class is_integral <bool>
+	{
+	public:
+		static const bool	value = true;
 	};
 
 	template <>
-	class is_integral < wchar_t > {
-		public:
-			static const bool	value = true;
+	class is_integral <char>
+	{
+	public:
+		static const bool	value = true;
 	};
 
 	template <>
-	class is_integral < int > {
-		public:
-			static const bool	value = true;
+	class is_integral <wchar_t>
+	{
+	public:
+		static const bool	value = true;
 	};
 
 	template <>
-	class is_integral < short > {
-		public:
-			static const bool	value = true;
+	class is_integral <int>
+	{
+	public:
+		static const bool	value = true;
 	};
 
 	template <>
-	class is_integral < long > {
-		public:
-			static const bool	value = true;
+	class is_integral <short>
+	{
+	public:
+		static const bool	value = true;
 	};
 
 	template <>
-	class is_integral < long long > {
-		public:
-			static const bool	value = true;
+	class is_integral <long>
+	{
+	public:
+		static const bool	value = true;
 	};
 
 	template <>
-	class is_integral < unsigned char > {
-		public:
-			static const bool	value = true;
+	class is_integral <long long>
+	{
+	public:
+		static const bool	value = true;
 	};
 
 	template <>
-	class is_integral < unsigned short > {
-		public:
-			static const bool	value = true;
+	class is_integral <unsigned char>
+	{
+	public:
+		static const bool	value = true;
 	};
 
 	template <>
-	class is_integral < unsigned int > {
-		public:
-			static const bool	value = true;
+	class is_integral <unsigned short>
+	{
+	public:
+		static const bool	value = true;
 	};
 
 	template <>
-	class is_integral < unsigned long > {
-		public:
-			static const bool	value = true;
+	class is_integral <unsigned int>
+	{
+	public:
+		static const bool	value = true;
 	};
 
 	template <>
-	class is_integral < unsigned long long > {
-		public:
-			static const bool	value = true;
+	class is_integral <unsigned long>
+	{
+	public:
+		static const bool	value = true;
 	};
 
-	template < typename _Type, typename _constType, bool isConst = false >
-	class typeSelector {
-		public:
-			typedef _Type	type;
+	template <>
+	class is_integral <unsigned long long>
+	{
+	public:
+		static const bool	value = true;
 	};
 
-	template < typename _Type, typename _constType>
-	class typeSelector< _Type, _constType, true > {
-		public :
-			typedef _constType	type;
+	template <typename Type, typename constType, bool isConst = false>
+	class typeSelector
+	{
+	public:
+		typedef Type	type;
 	};
 
-	template < typename _T >
-	class less {
-		public:
-			bool	operator() (const _T& x, const _T& y) const
-			{
-				return (x < y);
-			}
+	template <typename Type, typename constType>
+	class typeSelector<Type, constType, true>
+	{
+	public :
+		typedef constType	type;
 	};
 
-	template < typename _InputIterator1, typename _InputIterator2 >
-	bool	equal (_InputIterator1 first1, _InputIterator1 last1, _InputIterator2 first2) {
-		while (first1 != last1) {
+	template <typename T>
+	class less
+	{
+	public:
+		bool operator() (const T& x, const T& y) const
+		{
+			return (x < y);
+		}
+	};
+
+	template <typename InputIterator1, typename InputIterator2>
+	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
+	{
+		while (first1 != last1)
+		{
 			if (!(*first1 == *first2))
+			{
 				return (false);
+			}
 			++first1;
 			++first2;
 		}
-		
 		return (true);
 	}
 
-	template < typename _InputIterator1, typename _InputIterator2 >
-	bool	lexicographical_compare (_InputIterator1 first1, _InputIterator1 last1, _InputIterator2 first2, _InputIterator2 last2) {
-		while (first1 != last1) {
+	template <typename InputIterator1, typename InputIterator2>
+	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
+	{
+		while (first1 != last1)
+		{
 			if (first2 == last2 || *first2 < *first1)
+			{
 				return (false);
+			}
 			else if (*first1 < *first2)
+			{
 				return (true);
+			}
 			++first1;
 			++first2;
 		}
-
 		return (first2 != last2);
 	}
 }
